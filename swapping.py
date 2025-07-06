850b049240d5241d38c372d62192dd5aa422cd69
Zainab-Wani123 <zainabwani126@gmail.com>
2025-07-06 22:36:25 +0530

    Add files via upload

---
+++ swapping.py
@@ -0,0 +1,15 @@
+#input number from user
+#num1 = int(input("Enter the first number: "))
+x=1
+#num2 = int(input("Enter the second number: ")
+y=2
+#swap the numbers
+#num1, num2 = num2, num1 
+x,y=y,x
+#print the swapped numbers
+#print("After swapping:")
+#print("First number:", num1)
+print("First number:", x)
+#print("Second number:", num2)
+print("Second number:", y)
+# End of code
